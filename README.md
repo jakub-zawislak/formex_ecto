bab2327638a2b07e1f6ea3e9ad34d2f9d7d3ef93
Jakub Zawiślak <harry19911@gmail.com>
2017-06-25 10:49:14 +0200

    code moved from formex

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# formex_ecto
+# Formex Ecto
+
+Library that integrates Ecto with Formex.
+
+It also has an Ecto.Changeset validator adapter for those who want to easily migrate a project
+from old (< 0.5) Formex.
+
+# Instalation
+
+```elixir
+def deps do
+  [{:formex_ecto, "~> 0.1.0"}]
+end
+```
+
+`config/config.exs`
+```elixir
+config :formex,
+  repo: App.Repo
+```
+
+`web/web.ex`
+```elixir
+def controller do
+  quote do
+    use Formex.Ecto.Controller
+  end
+end
+```
+
+In every form type that uses Ecto:
+
+```elixir
+defmodule App.ArticleType do
+  use Formex.Type
+  use Formex.Ecto.Type
+```
+
+# Docs
+
+* [Usage in a controller](https://hexdocs.pm/formex_ecto/Formex.Ecto.Controller.html)
+* [Changeset validator adapter](https://hexdocs.pm/formex_ecto/Formex.Ecto.ChangesetValidator.html)
---
+++ mix.exs
@@ -0,0 +1,60 @@
+defmodule Formex.Ecto.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :formex_ecto,
+     version: "0.1.0",
+     elixir: "~> 1.3",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps(),
+     package: package(),
+     description: description(),
+     docs: [main: "readme",
+          extras: ["README.md"]],
+     source_url: "https://github.com/jakub-zawislak/formex_ecto",
+     elixirc_paths: elixirc_paths(Mix.env),
+     aliases: aliases()
+   ]
+  end
+
+  def application do
+    []
+  end
+
+  defp deps do
+    deps = [
+      {:ecto, "~> 2.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:postgrex, ">= 0.0.0", only: [:dev, :test]}, # without a :dev the jakub-zawislak/phoenix-forms won't start. maybe should be removed
+      {:phoenix, "~> 1.2", only: [:dev, :test]},
+      {:phoenix_ecto, "~> 3.0", only: [:dev, :test]}
+    ]
+
+    if !System.get_env("FORMEX_DEV") do
+      deps ++ [{:formex, "~> 0.5.0"}]
+    else
+      deps
+    end
+  end
+
+  defp description do
+    """
+    Formex Ecto
+    """
+  end
+
+  defp package do
+    [maintainers: ["Jakub Zawiślak"],
+     licenses: ["MIT"],
+     files: ~w(lib LICENSE.md mix.exs README.md),
+     links: %{github: "https://github.com/jakub-zawislak/formex_ecto"}]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp aliases do
+    ["test": ["ecto.migrate", "test"]]
+  end
+end
