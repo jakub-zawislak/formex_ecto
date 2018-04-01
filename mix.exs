7786c69264eafec0f76554604a73f0e3f1229d1a
Jakub Zawiślak <harry19911@gmail.com>
2018-04-01 23:53:17 +0200

    bumped ver

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Formex.Ecto.Mixfile do
 
   def project do
     [app: :formex_ecto,
-     version: "0.1.8",
+     version: "0.1.9",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
