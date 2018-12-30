82ab24a5cbe30d2ac825409c9a09a5f2a3471971
Jakub Zawiślak <harry19911@gmail.com>
2018-12-30 22:37:09 +0100

    ecto 3.0 support jakub-zawislak/formex#18

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -1,3 +1,6 @@
 defmodule Formex.Ecto.TestRepo do
-  use Ecto.Repo, otp_app: :formex_ecto
+  use Ecto.Repo,
+    otp_app: :formex_ecto,
+    # postgres is required for schema_embedded tests
+    adapter: Ecto.Adapters.Postgres
 end
